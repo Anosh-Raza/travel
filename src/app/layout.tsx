d5e5aef01e66e2c6c042bc19fda0b9a817bdd33e
Anosh-Raza <anoshraxa@gmail.com>
2024-04-05 02:30:53 +0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UIUX Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
+    </html>
+  );
+}
