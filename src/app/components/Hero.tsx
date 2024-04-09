d089906283d08cf53d7a3ef07809918ef35ffb4b
Anosh-Raza <anoshraxa@gmail.com>
2024-04-09 19:05:59 +0500

    Hero Sectio Button Image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -46,7 +46,7 @@ function Hero() {
         </div>
         <div className="flex flex-col w-full gap-3 sm:flex-row">
           <Button type="button" title="Download App" variant="btn_green" />
-          <Button type="button" title="How we work" icon="/play.svg" variant="btn_white_text" />
+          <Button type="button" title="How we work" icon="play.svg" variant="btn_white_text" />
         </div>
       </div>
       <div className="relative flex flex-1 items-start">
